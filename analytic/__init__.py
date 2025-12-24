e47fce19c409fb7cc51114fc36cd5cd85c195956
ddhendo <102411361+origi6615@users.noreply.github.com>
2025-12-24 18:27:29 +0800

    Update __init__.py

--- analytic/__init__.py
+++ analytic/__init__.py
@@ -3,13 +3,9 @@ from .Learner import Learner
 from .Buffer import Buffer, RandomBuffer, GaussianKernel
 from .AnalyticLinear import AnalyticLinear, RecursiveLinear
 from .ACIL import ACIL, ACILLearner
-from .DSAL import DSAL, DSALLearner
-from .CDSAL import CDSAL, CDSALLearner
 
-from .LACL import LACL, LACL
-from .GKEAL import GKEAL, GKEALLearner
-from .AEFOCL import AEFOCL, AEFOCLLearner
-from .AIR import AIRLearner, GeneralizedAIRLearner
+
+from .LACL import LACL, LACLLearner
 
 
 __all__ = [
